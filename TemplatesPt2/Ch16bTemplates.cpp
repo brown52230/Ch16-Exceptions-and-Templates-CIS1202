f41513823542db62763cb3391bb10b4df9a77172
brown52230 <brown52230@mvctc>
2022-04-21 10:54:26 -0400

    added new file, outlined sample data and functions

---
+++ TemplatesPt2/Ch16bTemplates.cpp
@@ -0,0 +1,17 @@
+//  Sam Brown
+//  CIS 1202 101
+//  April 21, 2022
+
+#include "SamPersonalLibrary.h"
+
+template <typename T>
+T half(T number);
+
+int main()
+{
+    double a = 7.0;
+    float b = 5.0f;
+    int c = 3;
+
+    endProgram();
+}
