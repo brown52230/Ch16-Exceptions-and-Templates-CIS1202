9210158c4e1882964b12a42c21b616f44eb933a4
brown52230 <brown52230@mvctc.com>
2022-04-21 14:57:51 -0400

    completed template ver

--- TemplatesPt2/Ch16bTemplates.cpp
+++ TemplatesPt2/Ch16bTemplates.cpp
@@ -13,5 +13,15 @@ int main()
     float b = 5.0f;
     int c = 3;
 
+    cout << half(a) << endl;
+    cout << half(b) << endl;
+    cout << half(c) << endl;
+
     endProgram();
+}
+
+template <typename T>
+T half(T number)
+{
+    return number / 2;
 }
